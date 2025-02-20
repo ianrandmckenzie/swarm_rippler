4bff74867cc8dc020d97a4f4bca3a1b9f0c2a865
Ian McKenzie <admin@ianrandmckenzie.com>
2025-02-20 14:31:16 -0800

    Add icons

--- electron/index.js
+++ electron/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, Menu, BrowserWindow, dialog, nativeImage } = require('electron');
 const path = require('path');
 
 let mainWindow;
@@ -25,18 +25,26 @@ app.on('window-all-closed', () => {
   }
 });
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    app.whenReady().then(() => {
-      mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-          nodeIntegration: true
-        }
-      });
+app.whenReady().then(() => {
+  const iconPath = process.platform === "darwin"
+    ? path.join(__dirname, "build/icons/icon.icns")
+    : process.platform === "win32"
+      ? path.join(__dirname, "build/icons/icon.ico")
+      : path.join(__dirname, "build/icons/icon.png");
 
-      mainWindow.loadFile(path.join(__dirname, 'index.html'));
-    });
-  }
+  console.log("Icon Path:", iconPath); // DEBUG: Print icon path to console
+  const icon = nativeImage.createFromPath(iconPath);
+  console.log("Icon isEmpty:", icon.isEmpty()); // DEBUG: Check if icon is loaded
+
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: icon,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
 });
+app.setName("psychoSounds");
