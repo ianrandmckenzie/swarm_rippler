624eb6e9d66d912a429cc74e2ff42200f9ff681a
Ian McKenzie <admin@ianrandmckenzie.com>
2025-02-20 15:02:24 -0800

    Get dock icon working

--- electron/index.js
+++ electron/index.js
@@ -1,12 +1,40 @@
-const { app, Menu, BrowserWindow, dialog, nativeImage } = require('electron');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 let mainWindow;
 
+app.name = 'psychSounds';
+
+const iconPngPath = path.resolve(__dirname, "build/icons/icon.png");
+const iconPath = process.platform === "darwin"
+? path.resolve(__dirname, "build/icons/icon.icns")
+: process.platform === "win32"
+  ? path.resolve(__dirname, "build/icons/icon.ico")
+  : iconPngPath;
+
+
 app.whenReady().then(() => {
+  app.dock.setIcon(iconPngPath);
+  createWindow();
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: iconPath,
     webPreferences: {
       nodeIntegration: true
     }
@@ -17,34 +45,5 @@ app.whenReady().then(() => {
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
-});
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.whenReady().then(() => {
-  const iconPath = process.platform === "darwin"
-    ? path.join(__dirname, "build/icons/icon.icns")
-    : process.platform === "win32"
-      ? path.join(__dirname, "build/icons/icon.ico")
-      : path.join(__dirname, "build/icons/icon.png");
-
-  console.log("Icon Path:", iconPath); // DEBUG: Print icon path to console
-  const icon = nativeImage.createFromPath(iconPath);
-  console.log("Icon isEmpty:", icon.isEmpty()); // DEBUG: Check if icon is loaded
-
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    icon: icon,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
-
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-});
-app.setName("psychoSounds");
+  app.setName("psychoSounds");
+};
