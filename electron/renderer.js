c92bc7945fafa3e9e8dd2d6fdfb18d1f2a5feb28
Curious Markings <exampleuser@Curiouss-MacBook-Air.local>
2025-07-07 15:11:15 -0700

    Convert stereo audio to mono, begin file cleanup

--- electron/renderer.js
+++
@@ -1,7 +0,0 @@
-function showView(viewId) {
-  document.querySelectorAll('.view').forEach(view => {
-      view.style.display = 'none';
-  });
-
-  document.getElementById(viewId).style.display = 'block';
-}
